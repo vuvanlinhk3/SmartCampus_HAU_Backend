926262ce8be8628827ed96c1ff20415999165535
nguyentuanhiep21 <145744239+nguyentuanhiep21@users.noreply.github.com>
2025-09-15 09:38:50 +0700

    add GetAllBookingsForStatistic

---
+++ SmartCampus_HAU_Backend/Models/DTOs/Bookings/AllBookingDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace SmartCampus_HAU_Backend.Models.DTOs.Bookings
+{
+    public class AllBookingDTO
+    {
+        public int BookingId { get; set; }
+        public int RoomId { get; set; }
+        public DateTime BookingDate { get; set; }
+    }
+}
